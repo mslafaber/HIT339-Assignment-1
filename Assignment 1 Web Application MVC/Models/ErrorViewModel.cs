6bbc057c9fd61f162d645d57c4cedf1eb4ea6c4a
Melisha <mslafaber1@gmail.com>
2020-08-12 19:27:10 +0930

    Created an MVC application using Individual User Authentication.

---
+++ Assignment 1 Web Application MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Assignment_1_Web_Application_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Assignment 1 Web Application MVC/Models/items.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_1_Web_Application_MVC.Models
+{
+    public class items
+    {
+    }
+}
