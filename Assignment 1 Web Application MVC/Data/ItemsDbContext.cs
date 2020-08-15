f47da888cb23557c79509753676c71d66c669f49
Melisha <mslafaber1@gmail.com>
2020-08-15 23:09:45 +0930

    Extra files to commit

---
+++ Assignment 1 Web Application MVC/Data/ItemsDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Assignment_1_Web_Application_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_1_Web_Application_MVC.Data
+{
+    public class ItemsDbContext : DbContext
+    {
+        public ItemsDbContext(DbContextOptions<ItemsDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Items> Items { get; set; }
+
+        public DbSet<Sales> Sales { get; set; }
+    }
+}
---
+++ Assignment 1 Web Application MVC/Migrations/20200815121738_createditemsandsalesClass.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Assignment_1_Web_Application_MVC.Migrations
+{
+    public partial class createditemsandsalesClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sales",
+                columns: table => new
+                {
+                    SalesId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Salesname = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sales", x => x.SalesId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Sales");
+        }
+    }
+}
