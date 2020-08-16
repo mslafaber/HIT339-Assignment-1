68aa32894a8bf8381a68f4ee6091dd380ba372e2
Melisha <mslafaber1@gmail.com>
2020-08-16 11:32:30 +0930

    Updated the steps word file and binded the required fields in the Items controller folder.

---
+++ Assignment 1 Web Application MVC/Migrations/20200816013617_addFieldstoItems.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Assignment_1_Web_Application_MVC.Migrations
+{
+    public partial class addFieldstoItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItemName",
+                table: "Items");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Items",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Items",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Items",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Items",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Items");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ItemName",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
